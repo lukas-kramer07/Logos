b51bb70d3a50590e32be0cfacd9ee4e52fded720
tomytrbl <doan.tomy@yahoo.com>
2025-04-23 21:25:05 +0200

    backend file structure spring boot with depencies: Spring Web, Data JPA, PostgreSQL, Spring Devtools

---
+++ LogosBackend/src/main/java/logos/demo/LogosBackendApplication.java
@@ -0,0 +1,13 @@
+package logos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogosBackendApplication.class, args);
+	}
+
+}
